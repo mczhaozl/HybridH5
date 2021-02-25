ff05acef93362834a9f1271d4ed17f2cbe047dfd
mczhaozl <2575630942@qq.com>
2021-02-25 16:24:08 +0800

    [feat] 底部分页导航

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'lib-flexible/flexible'
-import MintUi from 'mint-ui'
-import 'mint-ui/lib/style.css'
-Vue.use(MintUi)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
